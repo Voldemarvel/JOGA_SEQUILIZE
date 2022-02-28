51186fed70f471540988cb37af645a92d67d0a75
Voldemarvel <75370381+Voldemarvel@users.noreply.github.com>
2022-02-28 09:48:07 +0200

    created connection to database
    ylesanne 14.02

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true})); 
 
+//connect to database
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+
+//testing connection
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch(err => {
+        console.error('unable to connect to the database', err);
+    });
+
 app.get("/", (req, res) => {
     res.json({ message: "welcome to sequelize application"});
 });
